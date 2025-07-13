b7539749af7b6191c98b3cb730adabad3a837af9
Sergii <dykyy.s@gmail.com>
2025-07-13 14:49:11 +0300

    start

--- components/GeolocationChecker/GeolocationChecker.tsx
+++ components/GeolocationChecker/GeolocationChecker.tsx
@@ -3,24 +3,29 @@
 import { useEffect } from 'react';
 
 import { getUserInfo } from '@/lib/service/opencagedataApi';
+import { useCurrencyStore } from '@/lib/stores/currencyStore';
 
 export default function GeolocationChecker() {
+  const { setBaseCurrency, baseCurrency, hasHydrated } = useCurrencyStore();
+
+  console.log('baseCurrency', baseCurrency);
+
   useEffect(() => {
+    if (!hasHydrated || baseCurrency) return;
     const options = {
       enableHighAccuracy: true,
       timeout: 5000,
       maximumAge: 0,
     };
-
     const success = async ({ coords }: GeolocationPosition) => {
       const data = await getUserInfo(coords);
-      return data.results[0].annotations.currency.iso_code;
+      setBaseCurrency(data.results[0].annotations.currency.iso_code);
     };
-
-    const error = () => {};
-
+    const error = () => {
+      setBaseCurrency('USD');
+    };
     navigator.geolocation.getCurrentPosition(success, error, options);
-  }, []);
+  }, [setBaseCurrency, baseCurrency, hasHydrated]);
 
   return null;
 }
--- lib/stores/currencyStore.ts
+++ lib/stores/currencyStore.ts
@@ -1,1 +1,28 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
 // lib\stores\currencyStore.ts
+type CurrencyState = {
+  baseCurrency: string;
+  hasHydrated: boolean;
+  setBaseCurrency: (currency: string) => void;
+  setHasHydrated: (state: boolean) => void;
+};
+
+export const useCurrencyStore = create<CurrencyState>()(
+  persist(
+    (set) => ({
+      baseCurrency: '',
+      hasHydrated: false,
+      setBaseCurrency: (currency) => set({ baseCurrency: currency }),
+      setHasHydrated: (state) => set({ hasHydrated: state }),
+    }),
+    {
+      name: 'baseCurrency',
+      partialize: (state) => ({ baseCurrency: state.baseCurrency }),
+      onRehydrateStorage: () => (state) => {
+        state?.setHasHydrated(true);
+      },
+    }
+  )
+);
